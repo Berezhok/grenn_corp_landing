f8c8bc0503df74d505787fec380e1624c27fd3e8
Berezhok <96240926+Berezhok@users.noreply.github.com>
2022-02-24 15:56:41 +0300

    last version

---
+++ js/animation.js
@@ -0,0 +1,94 @@
+
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+const STEP = 100;
+
+function increaseNumberAnimationStep (i, element, endNumber) {
+    
+    if (i <= endNumber) {
+      if (i === endNumber) {
+        element.innerText = i + '+';
+      } else {
+        element.innerText = i;
+        i += STEP;
+        setTimeout(function () {increaseNumberAnimationStep (i, element, endNumber);}, INCREASE_NUMBER_ANIMATION_SPEED);
+      }
+    }
+ }
+     
+
+  function initIncreaseNumberAnimation() {
+   const element=document.querySelector(".features__clients-count");
+   increaseNumberAnimationStep (0, element, 5000);
+  }
+  
+  // initIncreaseNumberAnimation();
+
+  document.querySelector('#many').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+      const formContainer = document.createElement('div');
+      formContainer.classList.add('form__group');
+      formContainer.classList.add('form__other-input'); // Задание 1
+   
+      const input = document.createElement('input');
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text"; // Задание 2
+        
+      formContainer.appendChild(input);
+      document.querySelector('#contact form').insertBefore(formContainer, document.querySelector('.form__submit')); // Задание 3
+    }
+   
+    const otherInput = document.querySelector('.form__other-input');
+    if (event.target.value !== 'other' && otherInput) { // Задание 5
+    document.querySelector('#contact form').removeChild(otherInput); // Задание 4
+    }
+  });
+
+
+
+  
+  let animationInited = false;
+  window.addEventListener('scroll', updateScroll);
+    function updateScroll(){
+      if(window.scrollY > 0){
+    const header = document.querySelector("header");
+    header.classList.add("header__scrolled");
+       }
+    else{
+        let header=document.querySelector("header");
+        header.classList.remove("header__scrolled");
+    }
+    
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if(windowBottomPosition>= countElementPosition && !animationInited){
+      animationInited = true;
+      initIncreaseNumberAnimation();
+    }
+
+    // let windowBottomPosition = window.scrollY + window.innerHeight;
+    // let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  }
+
+ 
+
+  // переходы с кнопок плавные
+
+  function addSmoothScroll(anchor) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+   
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  }
+   
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    addSmoothScroll(anchor);
+  });
+
+
+  addSmoothScroll(document.querySelector('.more-button'));
+
+
+  
